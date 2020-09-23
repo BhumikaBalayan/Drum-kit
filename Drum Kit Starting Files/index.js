e5684d44357bb56b4a2215248c452a853d6d711f
Bhumika Balayan <balayanbhumika@gmail.com>
2020-09-23 21:54:24 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,75 @@
+    //Detects Button Press
+var drumClick = document.querySelectorAll(".drum").length;
+
+for( var i=0 ; i<drumClick ; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+var buttoninnerHTML = this.innerHTML;
+
+MakeSound(buttoninnerHTML);
+ButtonAnimation(buttoninnerHTML);
+
+  });
+}
+
+  //Detects Keyboard Press
+document.addEventListener("keydown", function(event){
+  MakeSound(event.key);
+  ButtonAnimation(event.key)
+});
+
+  //Relevant Sound
+function MakeSound(key) {
+    switch (key) {
+      case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+
+      case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+
+      case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+
+      case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+
+      case "j":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+
+      case "k":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+
+      case "l":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+
+      default: console.log(buttoninnerHTML);
+
+    }
+  }
+
+
+function ButtonAnimation(currentkey) {
+  var activebutton = document.querySelector("." + currentkey);
+  activebutton.classList.add("pressed");
+
+  setTimeout(function(){
+    activebutton.classList.remove("pressed");
+  }, 125)
+
+
+}
